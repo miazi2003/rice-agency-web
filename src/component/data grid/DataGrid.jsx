caf3ebeb973e9c09a58da0ffeffdd4a316558b99
miazi2003 <yeasinmiazi1997@gmail.com>
2025-10-04 00:49:16 +0600

    data grid updated

--- src/component/data grid/DataGrid.jsx
+++ src/component/data grid/DataGrid.jsx
@@ -10,7 +10,7 @@ const NotificationsTable = () => {
   useEffect(() => {
     const fetchNotifications = async () => {
       try {
-        const res = await axios.get("http://localhost:3000/notifications");
+        const res = await axios.get("http://localhost:5000/notifications");
         setNotifications(res.data);
       } catch (err) {
         console.error("Error fetching notifications:", err);
